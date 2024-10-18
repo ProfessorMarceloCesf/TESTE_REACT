17f384953d59431fe5a381d121c8c5caef3c6ceb
Professor Marcelo <avalia.cesf.marcelo2024@gmail.com>
2024-10-18 10:52:10 -0300

    subir teste react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Nome from './components/Nome';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Nosso primeiro app em React</h1>
+      
+      <h3>Usando componentes</h3>
+
+      <Nome pessoa ='Marcelo'/>
+      <br/>
+      <Nome pessoa ='Rubens'/>
+      
     </div>
   );
 }
